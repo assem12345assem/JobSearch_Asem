7c7f13c9dfc7032c0dcb2e464d95cff920062c52
asem <assemsim@gmail.com>
2023-07-05 13:46:40 +0600

    Employer resume model

---
+++ src/main/java/com/example/jobsearch/model/EmployerResume.java
@@ -0,0 +1,10 @@
+package com.example.jobsearch.model;
+
+public class EmployerResume {
+    private int id;
+    private String vacancyName;
+    private Integer salary;
+    private String description;
+    private String requiredJobExperience;
+    private String category;
+}
