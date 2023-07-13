2e59ea3bbee31b74405cbdf98bbfc4639f90c1d7
asem <assemsim@gmail.com>
2023-07-13 18:59:35 +0600

    create & insert: all db tables and data

--- src/main/java/com/example/jobsearch/service/UserService.java
+++ src/main/java/com/example/jobsearch/service/UserService.java
@@ -17,19 +17,19 @@ public class UserService {
         return userDao.getAllUsers();
     }
 
-    public void someMethod(int userId) {
-        Optional<User> mayByUser = userDao.getOptionalUserById(userId);
-        mayByUser.ifPresent(e -> System.out.printf("%s, %s, %s%n", e.getId(), e.getLastName(), e.getPassword()));
-    }
+//    public void someMethod(int userId) {
+//        Optional<User> mayByUser = userDao.getOptionalUserById(userId);
+//        mayByUser.ifPresent(e -> System.out.printf("%s, %s, %s%n", e.getId(), e.getLastName(), e.getPassword()));
+//    }
     public Optional<User> getUserById(int id) {
         return userDao.getOptionalUserById(id);
     }
-    public Optional<User> getUserByFirstName(String firstName) {
-        return userDao.getOptionalUserByFirstName(firstName);
-    }
-    public Optional<User> getOptionalUserByLastName(String lastName) {
-        return userDao.getOptionalUserByLastName(lastName);
-    }
+//    public Optional<User> getUserByFirstName(String firstName) {
+//        return userDao.getOptionalUserByFirstName(firstName);
+//    }
+//    public Optional<User> getOptionalUserByLastName(String lastName) {
+//        return userDao.getOptionalUserByLastName(lastName);
+//    }
     public Optional<User> getOptionalUserByEmail(String email) {
         return userDao.getOptionalUserByEmail(email);
     }
